3885cc0136f99b19863e9f21b2f3636711eb6711
deefyouknow <deefyouknow@gmail.com>
2025-11-06 12:25:39 +0700

    deef

--- src/pages/PageA.tsx
+++ src/pages/PageA.tsx
@@ -3,7 +3,7 @@ import Headeriot from '../components/Headeriot';
 export default function PageA() {
   return (
     <div>
-      <Headeriot wow="หน้า A" woo="หน้า A" />
+      <Headeriot />
       <h1>Welcome to Page A</h1>
     </div>
   );
